d5f41720dfb55c62a4d51b8f7e7e3fe5beabf79a
Miaoren Xia <xiamr@ihep.ac.cn>
2019-10-24 20:46:45 +0800

    speed optimization for rotational auto-correlation function calculation

--- src/analysis/RotAcf.cpp
+++ src/analysis/RotAcf.cpp
@@ -15,8 +15,6 @@
 #include "VectorSelectorFactory.hpp"
 #include "LegendrePolynomial.hpp"
 
-using namespace std;
-
 void RotAcf::processFirstFrame(std::shared_ptr<Frame> &frame) {
     rots.resize(vectorSelector->initialize(frame));
 }
@@ -39,19 +37,19 @@ void RotAcf::print(std::ostream &os) {
             {4, [this] { return calculate(LegendrePolynomialLevel4()); }}
     };
     auto acf = func_mapping.at(LegendrePolynomial)();
-    vector<double> integration = integrate(acf);
+    auto integration = integrate(acf);
     os << description() << '\n';
     os << "    Time Gap      ACF               integrate\n";
     os << "      (ps)                            (ps)\n";
-    for (std::size_t t : boost::irange(acf.size() + 1)) {
+    for (auto t : boost::irange(acf.size())) {
         os << boost::format("%12.2f%18.14f%15.5f\n") % (t * time_increment_ps) % acf[t] % integration[t];
     }
 }
 
-vector<double> RotAcf::integrate(const vector<double> &acf) const {
-    vector<double> integrate(acf.size());
+std::vector<double> RotAcf::integrate(const std::vector<double> &acf) const {
+    std::vector<double> integrate(acf.size());
     integrate[0] = 0.0;
-    for (size_t i = 1; i < integrate.size(); i++) {
+    for (auto i  : boost::irange(std::size_t(1), integrate.size())) {
         integrate[i] = integrate[i - 1] + 0.5 * (acf[i - 1] + acf[i]) * time_increment_ps;
     }
     return integrate;
@@ -60,57 +58,45 @@ vector<double> RotAcf::integrate(const vector<double> &acf) const {
 template<typename Function>
 class RotAcfParallelBody {
 public:
-    const vector<vector<tuple<double, double, double>>> &rots;
-    double *acf = nullptr;
-    unsigned long long *ntime = nullptr;
+    const std::vector<std::vector<std::tuple<double, double, double>>> &rots;
+    std::vector<double, tbb::tbb_allocator<double>> acf;
 
     size_t array_length;
     size_t max_time_grap_step;
     Function f;
 
-    explicit RotAcfParallelBody(const vector<vector<tuple<double, double, double>>> &rots,
+    explicit RotAcfParallelBody(const std::vector<std::vector<std::tuple<double, double, double>>> &rots,
                                 size_t max_time_grap_step, size_t array_length, Function f)
-            : rots(rots), acf(new double[array_length]{}),
-              ntime(new unsigned long long[array_length]{}),
+            : rots(rots), acf(array_length),
               array_length(array_length),
               max_time_grap_step(max_time_grap_step), f(f) {}
 
     RotAcfParallelBody(const RotAcfParallelBody &rhs, tbb::split)
-            : rots(rhs.rots), acf(new double[rhs.array_length]{}),
-              ntime(new unsigned long long[rhs.array_length]{}),
+            : rots(rhs.rots), acf(rhs.array_length),
               array_length(rhs.array_length),
               max_time_grap_step(rhs.max_time_grap_step), f(rhs.f) {}
 
     void join(const RotAcfParallelBody &rhs) {
         for (size_t i = 1; i < array_length; i++) {
             acf[i] += rhs.acf[i];
-            ntime[i] += rhs.ntime[i];
         }
     }
 
-    virtual ~RotAcfParallelBody() {
-        delete[] acf;
-        delete[] ntime;
-    }
-
-    void operator()(const tbb::blocked_range<int> &range) {
+    void operator()(const tbb::blocked_range<std::size_t> &range) {
         for (auto index = range.begin(); index != range.end(); index++) {
             auto &_vector = rots[index];
             auto total_size = _vector.size();
 
             for (size_t i = 0; i < total_size - 1; i++) {
-                for (size_t j = i + 1; j < total_size; j++) {
+                for (size_t j = i + 1; j < std::min(total_size, max_time_grap_step + i); j++) {
                     auto m = j - i;
 
                     assert(i < _vector.size());
                     assert(j < _vector.size());
 
-                    if (m > max_time_grap_step) break;
-
                     double cos = dot_multiplication(_vector[i], _vector[j]);
 
                     acf[m] += f(cos);
-                    ntime[m]++;
                 }
             }
         }
@@ -118,17 +104,19 @@ public:
 };
 
 template<typename Function>
-vector<double> RotAcf::calculate(Function f) const {
-    size_t max_time_grap_step = std::ceil(max_time_grap / time_increment_ps);
+std::vector<double> RotAcf::calculate(Function f) const {
+    size_t max_time_grap_step = std::ceil(max_time_grap / time_increment_ps) + 1;
 
-    RotAcfParallelBody parallelBody(rots, max_time_grap_step, min(rots[0].size(), max_time_grap_step + 1), f);
+    auto array_length = std::min(rots[0].size(), max_time_grap_step);
+    RotAcfParallelBody parallelBody(rots, max_time_grap_step, array_length, f);
 
-    tbb::parallel_reduce(tbb::blocked_range<int>(0, rots.size()), parallelBody, tbb::auto_partitioner());
+    tbb::parallel_reduce(tbb::blocked_range(std::size_t(0), rots.size()), parallelBody);
 
-    std::vector<double> acf(max_time_grap_step + 1, 0);
-    for (size_t i = 1; i <= max_time_grap_step; i++) {
-        assert(parallelBody.ntime[i] > 0);
-        acf[i] = parallelBody.acf[i] / parallelBody.ntime[i];
+    std::vector<double> acf(array_length);
+    auto total_mols = rots.size();
+    auto total_size = rots[0].size();
+    for (size_t i = 1; i < array_length; i++) {
+        acf[i] = parallelBody.acf[i] / total_mols / (total_size - i);
     }
 
     acf[0] = 1.0;
@@ -141,7 +129,7 @@ void RotAcf::readInfo() {
     vectorSelector->readInfo();
     std::cout << "Legendre Polynomial\n";
     boost::range::for_each(boost::irange<int>(1, LegendreStr.size() + 1),
-                           [](auto i) { cout << to_string(i) << ". " << LegendreStr.at(i) << '\n'; });
+                           [](auto i) { std::cout << std::to_string(i) << ". " << LegendreStr.at(i) << '\n'; });
 
     LegendrePolynomial = choose<int>(1, LegendreStr.size(), "select > ");
 
@@ -151,46 +139,46 @@ void RotAcf::readInfo() {
     max_time_grap = choose(0.0, std::numeric_limits<double>::max(), "Enter the Max Time Grap in Picoseconds :");
 }
 
-void RotAcf::setParameters(const shared_ptr<VectorSelector> &vector, int LegendrePolynomial,
-                           double time_increment_ps, double max_time_grap_ps, const string &outfilename) {
+void RotAcf::setParameters(const std::shared_ptr<VectorSelector> &vector, int LegendrePolynomial,
+                           double time_increment_ps, double max_time_grap_ps, const std::string &outfilename) {
 
     vectorSelector = vector;
     if (!(LegendrePolynomial >= 1 and LegendrePolynomial <= LegendreStr.size())) {
-        throw runtime_error("legendre polynomial must be in the rang ef 1.." + to_string(LegendreStr.size()));
+        throw std::runtime_error("legendre polynomial must be in the rang ef 1.." + std::to_string(LegendreStr.size()));
     }
 
     this->LegendrePolynomial = LegendrePolynomial;
 
     if (time_increment_ps <= 0) {
-        throw runtime_error("`time_increment_ps` must be postive");
+        throw std::runtime_error("`time_increment_ps` must be postive");
     } else {
         this->time_increment_ps = time_increment_ps;
     }
 
     if (max_time_grap_ps <= 0) {
-        throw runtime_error("`max_time_grap_ps` must be postive");
+        throw std::runtime_error("`max_time_grap_ps` must be postive");
     } else if (max_time_grap_ps <= this->time_increment_ps) {
-        throw runtime_error("`max_time_grap_ps` must be larger than `time_increment_ps`");
+        throw std::runtime_error("`max_time_grap_ps` must be larger than `time_increment_ps`");
     } else {
         this->max_time_grap = max_time_grap_ps;
     }
     this->outfilename = outfilename;
     boost::trim(this->outfilename);
     if (this->outfilename.empty()) {
-        throw runtime_error("outfilename cannot empty");
+        throw std::runtime_error("outfilename cannot empty");
     }
 }
 
-string RotAcf::description() {
-    stringstream ss;
-    string title_line = "------ " + title() + " ------";
+std::string RotAcf::description() {
+    std::stringstream ss;
+    std::string title_line = "------ " + title() + " ------";
     ss << title_line << "\n";
     ss << " vector            = " << vectorSelector->description() << "\n";
     ss << " P                 = " << LegendrePolynomial << "  [ " << LegendreStr.at(LegendrePolynomial) << " ] \n";
     ss << " time_increment_ps = " << time_increment_ps << " (ps)\n";
     ss << " max_time_grap_ps  = " << max_time_grap << " (ps)\n";
     ss << " outfilename       = " << outfilename << "\n";
-    ss << string(title_line.size(), '-') << '\n';
+    ss << std::string(title_line.size(), '-') << '\n';
     return ss.str();
 }
 
