90cd8c5cda2a56982ac4f76a91a683d2f5f5df7f
Miaoren Xia <xiamr@ihep.ac.cn>
2019-12-02 12:03:27 +0800

    class Averager refactor

--- src/analysis/others/Averager.cpp
+++ src/analysis/others/Averager.cpp
@@ -1,5 +1,4 @@
 
-#include <boost/algorithm/string.hpp>
 #include <boost/range/algorithm.hpp>
 #include "Averager.hpp"
 #include "utils/common.hpp"
@@ -21,9 +20,42 @@ void Averager::process() {
         exit(EXIT_FAILURE);
     }
 
+    int range = choose(2, "Enter range (line) > ");
+
+    process_file(ifs, ofs, range);
+
+}
+
+void Averager::process_file(std::ifstream &ifs, std::ofstream &ofs, const int range) {
+
+    std::vector<std::vector<double>> lines = readfile(ifs);
+
+    output_average(ofs, range, lines);
+}
+
+void Averager::output_average(std::ostream &os, int range, const std::vector<std::vector<double>> &lines) {
+
+    std::vector<double> avg;
+    avg.resize(lines.front().size());
+    for (std::size_t i = 0; i < lines.size(); ++i) {
+        const auto &v = lines[i];
+        for (std::size_t j = 0; j < v.size(); ++j) {
+            avg[j] += v[j];
+        }
+
+        if ((i + 1) % range == 0) {
+            for (auto value : avg) {
+                os << (value / range) << "  ";
+            }
+            os << '\n';
+            boost::fill(avg, 0.0);
+        }
+    }
+}
+
+std::vector<std::vector<double>> Averager::readfile(std::ifstream &ifs) {
 
     std::vector<std::vector<double>> lines;
-
     std::string line;
 
     while (!ifs.eof()) {
@@ -38,23 +70,5 @@ void Averager::process() {
         }
         lines.emplace_back(std::move(item));
     }
-
-    int range = choose(2, "Enter range (line) > ");
-
-    std::vector<double> avg;
-    avg.resize(lines.front().size());
-    for (std::size_t i = 0; i < lines.size(); ++i) {
-        const auto &v = lines[i];
-        for (std::size_t j = 0; j < v.size(); ++j) {
-            avg[j] += v[j];
-        }
-
-        if ((i + 1) % range == 0) {
-            for (auto value : avg) {
-                ofs << (value / range) << "  ";
-            }
-            ofs << '\n';
-            boost::fill(avg, 0.0);
-        }
-    }
+    return lines;
 }
--- src/analysis/others/Averager.hpp
+++ src/analysis/others/Averager.hpp
@@ -3,8 +3,9 @@
 #ifndef TINKER_AVERAGER_HPP
 #define TINKER_AVERAGER_HPP
 
-
+#include <iosfwd>
 #include <string_view>
+#include <vector>
 
 class Averager {
 public:
@@ -12,6 +13,12 @@ public:
     static void process();
 
     [[nodiscard]] static std::string_view title() { return "Averager"; }
+
+    static void process_file(std::ifstream &ifs, std::ofstream &ofs, int range);
+
+    [[nodiscard]] static std::vector<std::vector<double>> readfile(std::ifstream &ifs);
+
+    static void output_average(std::ostream &os, int range, const std::vector<std::vector<double>> &lines);
 };
 
 
