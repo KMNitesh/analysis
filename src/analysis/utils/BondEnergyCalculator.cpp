aa9e3f1229ab37d010bc027f486918c63ff5f9e5
Miaoren Xia <xiamr@ihep.ac.cn>
2020-03-26 16:24:21 +0800

    bonded energy calculator refactor

--- src/analysis/utils/BondEnergyCalculator.cpp
+++ src/analysis/utils/BondEnergyCalculator.cpp
@@ -45,17 +45,17 @@ BondEnergyCalculator::Term BondEnergyCalculator::energy(const std::shared_ptr<Fr
 
     Term term{};
     for (auto it = bonds.begin(); it != bonds.end(); ++it) {
-        const auto [atoms, param] = **it;
+        const auto &[atoms, param] = **it;
         auto r = atom_distance(atoms, frame) - param.rA;
         term.bond += 0.5 * param.krA * r * r;
     }
     for (auto it = angles.begin(); it != angles.end(); ++it) {
-        const auto [atoms, param] = **it;
+        const auto &[atoms, param] = **it;
         auto theta = (atom_angle(atoms, frame) - param.rA) / radian;
         term.angle += 0.5 * param.krA * theta * theta;
     }
     for (auto it = dihedrals.begin(); it != dihedrals.end(); ++it) {
-        const auto [atoms, param] = **it;
+        const auto &[atoms, param] = **it;
         auto cos = std::cos((atom_dihedral(atoms, frame) * param.mult - param.phiA) / radian);
         term.dihedral += param.cpA * (1 + cos);
     }
