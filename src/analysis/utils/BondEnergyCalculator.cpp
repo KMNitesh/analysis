1a817d1e26fe551fa1b5b735b08cf0053b8151b3
Miaoren Xia <xiamr@ihep.ac.cn>
2020-03-26 16:18:08 +0800

    bonded energy calculator refactor

--- src/analysis/utils/BondEnergyCalculator.cpp
+++ src/analysis/utils/BondEnergyCalculator.cpp
@@ -5,26 +5,34 @@
 #include "utils/common.hpp"
 #include <boost/range/algorithm.hpp>
 
+namespace {
+
+template <typename T, typename U> bool all_in_set(const T &sequecne, const std::set<U> &set) {
+    for (auto &i : sequecne) {
+        if (!set.contains(i))
+            return false;
+    }
+    return true;
+}
+} // namespace
+
 BondEnergyCalculator::BondEnergyCalculator(const std::vector<std::shared_ptr<Atom>> &atoms,
                                            const std::shared_ptr<Frame> &frame) {
 
     std::set atoms_set(std::begin(atoms), std::end(atoms));
 
     for (const auto &bond : frame->f_bond_params) {
-        if (atoms_set.contains(bond.first[0]) and atoms_set.contains(bond.first[1])) {
+        if (all_in_set(bond.first, atoms_set)) {
             bonds.push_back(&bond);
         }
     }
-
     for (const auto &angle : frame->f_angle_params) {
-        if (atoms_set.contains(angle.first[0]) and atoms_set.contains(angle.first[1]) and
-            atoms_set.contains(angle.first[2])) {
+        if (all_in_set(angle.first, atoms_set)) {
             angles.push_back(&angle);
         }
     }
     for (const auto &dihedral : frame->f_dihedral_params) {
-        if (atoms_set.contains(dihedral.first[0]) and atoms_set.contains(dihedral.first[1]) and
-            atoms_set.contains(dihedral.first[2]) and atoms_set.contains(dihedral.first[3])) {
+        if (all_in_set(dihedral.first, atoms_set)) {
             dihedrals.push_back(&dihedral);
         }
     }
@@ -41,7 +49,6 @@ BondEnergyCalculator::Term BondEnergyCalculator::energy(const std::shared_ptr<Fr
         auto r = atom_distance(atoms, frame) - param.rA;
         term.bond += 0.5 * param.krA * r * r;
     }
-
     for (auto it = angles.begin(); it != angles.end(); ++it) {
         const auto [atoms, param] = **it;
         auto theta = (atom_angle(atoms, frame) - param.rA) / radian;
