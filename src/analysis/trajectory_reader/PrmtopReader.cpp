66ef33c33e3105f65cdd8f8f9b0ce4105c8e6a50
Miaoren Xia <xiamr@ihep.ac.cn>
2020-04-01 08:53:11 +0800

    read bonded paramater from prmtop

--- src/analysis/trajectory_reader/PrmtopReader.cpp
+++ src/analysis/trajectory_reader/PrmtopReader.cpp
@@ -56,7 +56,7 @@ std::shared_ptr<Frame> PrmtopReader::read(const std::string &filename) {
 
     auto all_bonds = join(prmtop->bonds_inc_hydrogen, prmtop->bonds_without_hydrogen);
     for (auto it = std::begin(all_bonds); it != std::end(all_bonds); it += 3) {
-        auto atom_num1 = (*it) / 3 + 1;
+        auto atom_num1 = (*(it + 0)) / 3 + 1;
         auto atom_num2 = (*(it + 1)) / 3 + 1;
 
         auto &atom1 = frame->atom_map[atom_num1];
@@ -64,6 +64,56 @@ std::shared_ptr<Frame> PrmtopReader::read(const std::string &filename) {
 
         atom1->con_list.push_back(atom_num2);
         atom2->con_list.push_back(atom_num1);
+
+        auto bond_index = *(it + 2);
+
+        frame->f_bond_params.emplace(std::array{atom1, atom2},
+                                     Frame::harmonic{.krA = prmtop->bond_force_constant[bond_index],
+                                                     .rA = prmtop->bond_equil_value[bond_index]});
+    }
+
+    auto all_angles = join(prmtop->angles_inc_hydrogen, prmtop->angles_without_hydrogen);
+    for (auto it = std::begin(all_angles); it != std::end(all_angles); it += 4) {
+        auto atom_num1 = (*(it + 0)) / 3 + 1;
+        auto atom_num2 = (*(it + 1)) / 3 + 1;
+        auto atom_num3 = (*(it + 2)) / 3 + 1;
+
+        auto angle_index = *(it + 3);
+
+        auto &atom1 = frame->atom_map[atom_num1];
+        auto &atom2 = frame->atom_map[atom_num2];
+        auto &atom3 = frame->atom_map[atom_num3];
+
+        frame->f_angle_params.emplace(std::array{atom1, atom2, atom3},
+                                      Frame::harmonic{.krA = prmtop->angle_force_constant[angle_index],
+                                                      .rA = prmtop->angle_equil_value[angle_index]});
+    }
+
+    auto all_dihedrals = join(prmtop->dihedrals_inc_hydrogen, prmtop->dihedrals_without_hydrogen);
+    for (auto it = std::begin(all_dihedrals); it != std::end(all_dihedrals); it += 5) {
+        auto atom_num1 = (*(it + 0)) / 3 + 1;
+        auto atom_num2 = (*(it + 1)) / 3 + 1;
+        auto atom_num3 = (*(it + 2)) / 3 + 1;
+        auto atom_num4 = (*(it + 3)) / 3 + 1;
+
+        auto dihedral_index = *(it + 4);
+
+        auto &atom1 = frame->atom_map[atom_num1];
+        auto &atom2 = frame->atom_map[atom_num2];
+        auto &atom3 = frame->atom_map[std::abs(atom_num3)];
+        auto &atom4 = frame->atom_map[std::abs(atom_num4)];
+
+        std::array key{atom1, atom2, atom3, atom4};
+
+        Frame::pdihs param{.phiA = prmtop->dihedral_phase[dihedral_index],
+                           .cpA = prmtop->dihedral_force_constant[dihedral_index],
+                           .mult = std::abs(prmtop->dihedral_periodicity[dihedral_index])};
+
+        if (atom_num4 > 0) {
+            frame->f_dihedral_params.emplace(std::move(key), param);
+        } else {
+            frame->f_improper_dihedral_params.emplace(std::move(key), param);
+        }
     }
 
     topology_utils::assgin_atom_to_molecule(frame);
